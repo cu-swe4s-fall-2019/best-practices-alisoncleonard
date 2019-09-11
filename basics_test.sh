27aeede9a9de465cdd2aa572f3a5fbeb47ee8a6e
alisoncleonard <alisoncleonard@gmail.com>
2019-09-10 22:30:30 -0600

    fixed arguments, removed -e from test files

--- basics_test.sh
+++ basics_test.sh
@@ -1,17 +1,29 @@
+#!/bin/bash
+
 pycodestyle style.py
 
 pycodestyle get_column_stats.py
 
-(for i in `seq 1 100`; do 
-    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
+(for i in `seq 1 100`; do
+    echo "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
 done )> data.txt
 
-python get_column_stats.py data.txt 2
+python get_column_stats.py --file_name data.txt --column_number 2
 
 
 V=1
-(for i in `seq 1 100`; do 
-    echo -e "$V\t$V\t$V\t$V\t$V";
+(for i in `seq 1 100`; do
+    echo "$V\t$V\t$V\t$V\t$V";
 done )> data.txt
 
-python get_column_stats.py data.txt 2
+python get_column_stats.py --column_number 2 --file_name data.txt
+
+# test if file is empty
+# test if file not ints
+# test if call column outside of range
+
+# add exit codes for failed tests
+
+#(for i in `seq 1 100`; do
+#    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
+#done )> data.txt
