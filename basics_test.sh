5b278390009e209255eb8879b5553676b520d7df
alisoncleonard <alisoncleonard@gmail.com>
2019-09-12 09:27:28 -0600

    updated available tests and added printed text to screen

--- basics_test.sh
+++ basics_test.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+echo 'running test file basics_test.sh'
+
+echo 'checking style.py and get_column_stats.py adherance to PEP8 style guide'
+
 pycodestyle style.py
 
 pycodestyle get_column_stats.py
+
+echo 'testing exception for FileNotFoundError'
+
+python get_column_stats.py --file_name test.txt --column_number 2
+
+echo 'testing a file of random integers'
 
 (for i in `seq 1 100`; do
     echo "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
@@ -10,6 +20,11 @@ done )> data.txt
 
 python get_column_stats.py --file_name data.txt --column_number 2
 
+echo 'testing exception for IndexError, column does not exist'
+
+python get_column_stats.py --file_name data.txt --column_number 5
+
+echo 'testing a file of 1s'
 
 V=1
 (for i in `seq 1 100`; do
@@ -18,12 +33,18 @@ done )> data.txt
 
 python get_column_stats.py --column_number 2 --file_name data.txt
 
-# test if file is empty
-# test if file not ints
-# test if call column outside of range
+V='a'
+W=1
+(for i in `seq 1 100`; do
+    echo "$V\t$W\t$V\t$W\t$V";
+done )> data.txt
 
-# add exit codes for failed tests
+echo 'testing file with strings in a non-specified column'
 
-#(for i in `seq 1 100`; do
-#    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
-#done )> data.txt
+python get_column_stats.py --file_name data.txt --column_number 1
+
+echo 'testing exception for ValueError, column contains non integers'
+
+python get_column_stats.py --file_name data.txt --column_number 2
+
+echo 'tests complete'
