b69cc36d114a9d620262ffec175cbbab77ace3b7
alisoncleonard <alisoncleonard@gmail.com>
2019-09-12 09:10:12 -0600

    added documentation to get_column_stats

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,10 +1,35 @@
+"""Calculate the mean and standard deviation of a column of integers
+
+    * main - returns the mean and standard deviation of integers from a
+    specified column from an input file
+"""
+
 import math
 import argparse
 import sys
 
 
 def main():
-    parser = argparse.ArgumentParser(description='calc mean and stdev of'
+    """
+    Compute the mean and standard deviation of a column of integers.
+
+    Parameters
+    ----------
+    --filename: Name of the file
+        The input file to read, containing integers in columns
+
+    --column_number: The column number
+        An integer specifing which column number to compute
+
+    Returns
+    --------
+    mean
+        The mean of the column values
+
+    stdev
+        The standard deviation of the column values
+    """
+    parser = argparse.ArgumentParser(description='calc mean and stdev of '
                                      'a column', prog='get_column_stats')
 
     parser.add_argument('--file_name', type=str,
@@ -19,7 +44,7 @@ def main():
     col_num = args.column_number
 
     try:
-        f = open(file_name, 'r')
+        file = open(file_name, 'r')
     except FileNotFoundError:
         print('Could not find ' + file_name)
         sys.exit(1)
@@ -29,9 +54,16 @@ def main():
 
     V = []
 
-    for l in f:
-        A = [int(x) for x in l.split()]
-        V.append(A[col_num])
+    for line in file:
+        try:
+            A = [x for x in line.split()]
+            V.append(int(A[col_num]))
+        except ValueError:
+            print('Column contains non integer values, unable to process')
+            sys.exit(1)
+        except IndexError:
+            print('Column number ' + str(col_num) + ' does not exist')
+            sys.exit(1)
 
     mean = sum(V)/len(V)
 
