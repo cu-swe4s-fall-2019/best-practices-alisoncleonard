f6b50e4d71efc555564bbc8e755450980a1ae958
alisoncleonard <alisoncleonard@gmail.com>
2019-09-18 22:40:26 -0600

    added ssshtest framework

--- basics_test.sh
+++ basics_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
 echo 'running test file basics_test.sh'
 
 echo "checking python files' adherance to PEP8 style guide"
@@ -12,19 +15,34 @@ pycodestyle basics_test.py
 
 echo 'testing exception for FileNotFoundError'
 
-python get_column_stats.py --file_name test.txt --column_number 2
+run main python get_column_stats.py --file_name test.txt --column_number 2
+assert_in_stdout 'Could not find test.txt'
+assert_no_stderr
+assert_exit_code 1
 
 echo 'testing a file of random integers'
 
-(for i in `seq 1 100`; do
-    echo "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
-done )> data.txt
+A=$RANDOM
+B=$RANDOM
+C=$RANDOM
+D=$RANDOM
+E=$RANDOM
 
-python get_column_stats.py --file_name data.txt --column_number 2
+#(for i in `seq 1 100`; do
+#    echo "$A\t$B\t$C\t$D\t$E";
+#done )> data.txt
+
+#run main python get_column_stats.py --file_name data.txt --column_number 2
+#assert_in_stdout 'mean: '
+
+#python get_column_stats.py --file_name data.txt --column_number 2
 
 echo 'testing exception for IndexError, column does not exist'
 
-python get_column_stats.py --file_name data.txt --column_number 5
+run main python get_column_stats.py --file_name data.txt --column_number 5
+assert_in_stdout 'Column number 5 does not exist'
+assert_no_stderr
+assert_exit_code 1
 
 echo 'testing a file of 1s'
 
@@ -33,7 +51,10 @@ V=1
     echo "$V\t$V\t$V\t$V\t$V";
 done )> data.txt
 
-python get_column_stats.py --column_number 2 --file_name data.txt
+run main python get_column_stats.py --file_name data.txt --column_number 2
+assert_in_stdout 'mean: ' 1.0, 'stdev: ' 0.0
+assert_no_stderr
+assert_exit_code 0
 
 V='a'
 W=1
@@ -43,10 +64,16 @@ done )> data.txt
 
 echo 'testing file with strings in a non-specified column'
 
-python get_column_stats.py --file_name data.txt --column_number 1
+run main python get_column_stats.py --file_name data.txt --column_number 1
+assert_in_stdout 'mean: ' 1.0, 'stdev: ' 0.0
+assert_no_stderr
+assert_exit_code 0
 
 echo 'testing exception for ValueError, column contains non integers'
 
-python get_column_stats.py --file_name data.txt --column_number 2
+run main python get_column_stats.py --file_name data.txt --column_number 2
+assert_in_stdout 'Column contains non integer values, unable to process'
+assert_no_stderr
+assert_exit_code 1
 
 echo 'tests complete'
