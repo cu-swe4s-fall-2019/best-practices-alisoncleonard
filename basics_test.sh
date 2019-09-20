a31d2c2d76a6f61e17621403e8bab315af5bf180
alisoncleonard <alisoncleonard@gmail.com>
2019-09-19 18:37:28 -0600

    echo -e in data.txt generation to pass travis

--- basics_test.sh
+++ basics_test.sh
@@ -2,7 +2,7 @@
 
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
-source ssshtest 
+source ssshtest
 
 echo 'running test file basics_test.sh'
 
@@ -24,17 +24,17 @@ assert_exit_code 1
 echo 'testing a file of random integers'
 
 (for i in `seq 1 100`; do
-    echo "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
-done )> data.txt
+    echo -e "$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM\t$RANDOM";
+done )> data1.txt
 
-run test_random_file python get_column_stats.py --file_name data.txt --column_number 2
+run test_random_file python get_column_stats.py --file_name data1.txt --column_number 2
 assert_stdout
 assert_no_stderr
 assert_exit_code 0
 
 echo 'testing exception for IndexError, column does not exist'
 
-run test_column_index python get_column_stats.py --file_name data.txt --column_number 5
+run test_column_index python get_column_stats.py --file_name data1.txt --column_number 5
 assert_in_stdout 'Column number 5 does not exist'
 assert_no_stderr
 assert_exit_code 1
@@ -43,10 +43,10 @@ echo 'testing a file of 1s'
 
 V=1
 (for i in `seq 1 100`; do
-    echo "$V\t$V\t$V\t$V\t$V";
-done )> data.txt
+    echo -e "$V\t$V\t$V\t$V\t$V";
+done )> data2.txt
 
-run test_constant_file python get_column_stats.py --file_name data.txt --column_number 2
+run test_constant_file python get_column_stats.py --file_name data2.txt --column_number 2
 assert_in_stdout 'mean: 1.0, stdev: 0.0'
 assert_no_stderr
 assert_exit_code 0
@@ -54,19 +54,19 @@ assert_exit_code 0
 V='a'
 W=1
 (for i in `seq 1 100`; do
-    echo "$V\t$W\t$V\t$W\t$V";
-done )> data.txt
+    echo -e "$V\t$W\t$V\t$W\t$V";
+done )> data3.txt
 
 echo 'testing file with strings in a non-specified column'
 
-run test_unspecified_column_strings python get_column_stats.py --file_name data.txt --column_number 1
+run test_unspecified_column_strings python get_column_stats.py --file_name data3.txt --column_number 1
 assert_in_stdout 'mean: 1.0, stdev: 0.0'
 assert_no_stderr
 assert_exit_code 0
 
 echo 'testing exception for ValueError, column contains non integers'
 
-run test_specified_column_strings python get_column_stats.py --file_name data.txt --column_number 2
+run test_specified_column_strings python get_column_stats.py --file_name data3.txt --column_number 2
 assert_in_stdout 'Column contains non integer values, unable to process'
 assert_no_stderr
 assert_exit_code 1
